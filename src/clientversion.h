60c43c36c2bfa6e76dada1b1279424036a321c9c
Robert Pickles <bob@solutionconcepts.co.uk>
2022-09-06 16:03:38 +0100

    BSG-179
    Tor network update

--- src/clientversion.h
+++ src/clientversion.h
@@ -13,6 +13,15 @@
 #define CLIENT_VERSION_REVISION    4
 #define CLIENT_VERSION_BUILD       2
 
+
+static const int CLIENT_VERSION_RPC =
+    1000000 * CLIENT_VERSION_MAJOR  ///
+    + 10000 * CLIENT_VERSION_MINOR  ///
+    + 100 * CLIENT_VERSION_REVISION ///
+    + 1 * CLIENT_VERSION_BUILD;
+
+
+
 // Set to true for release, false for prerelease or test build
 #define CLIENT_VERSION_IS_RELEASE  true
 
